36d3b5fe4d567d5564adfa67e098df2b83c88631
Marcus <marcusfish2004@gmail.com>
2024-02-10 19:53:53 +0000

    added nav bar scroll feature

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,36 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 });
+
+let home = document.getElementById('home');
+
+home.onclick = function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+};
+
+let about = document.getElementById('about');
+let aboutSection = document.querySelector('.about-me');
+
+about.onclick = function() {
+    let yOffset = -100;
+    let y = aboutSection.getBoundingClientRect().top + pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+};
+
+let proj = document.getElementById('proj');
+let projectSection = document.querySelector('.projects');
+
+proj.onclick = function() {
+    let yOffset = -100;
+    let y = projectSection.getBoundingClientRect().top + pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth'});
+};
+
+let contact = document.getElementById('contact');
+let contactSection = document.querySelector('.Contact');
+
+contact.onclick = function() {
+    let yOffset = -100;
+    let y = contactSection.getBoundingClientRect().top + pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth'});
+}
